1cd19075fea1c4581d350a009227a6bce5a32889
pujapujan71-pixel <pujapujan71@gamil.com>
2026-04-23 17:09:22 +0900

    up

---
+++ main.js
@@ -0,0 +1,13 @@
+// const name = prompt("名前は？");
+// alert("こんにちは " + name +" さん !" );
+//textContent = "こんにちは  " + name + "  さん ! ";
+
+const target = document.getElementById("target"); 
+terget.textConten = "こんにちは";
+target .style.color = "red";
+target .style = "30px";
+target .style.backgroundColor = "yellow";
+
+
+
+
