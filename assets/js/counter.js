9532c76129f1b85ddf16e5356b9c88d6c6fd926f
pujapujan71-pixel <pujapujan71@gamil.com>
2026-04-30 16:17:34 +0900

    写真を切替しました

---
+++ assets/js/counter.js
@@ -0,0 +1,16 @@
+let counter = 0;
+
+function increment() { 
+    counter++;
+    document.getElementById('counter').textContent = counter;
+}
+
+function reset(){ 
+    counter = 0;
+    document.getElementById('counter').textContent = counter;
+}
+
+function decrement() { 
+    counter--;
+    document.getElementById('counter').textContent = counter;
+}
