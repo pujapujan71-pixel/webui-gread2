203f4d2270800b4b692e85386fd2fdc2fa462668
pujapujan71-pixel <pujapujan71@gamil.com>
2026-05-28 15:59:15 +0900

    formupdate

--- cafe/script.js
+++ cafe/script.js
@@ -1,10 +1,22 @@
 // form
 function reserve() {
-  const name = document.getElementById('guestName').value;
-  const count = document.getElementById('guestCount').value;
+    // 入力した内容を取得する  
+    const name = document.getElementById('guestName').value;
+    const count = document.getElementById('guestCount').value;
 
-  const result = document.getElementById('reserveResult');
+    // 結果を表示する場所を取得する
+    const result = document.getElementById('reserveResult');
 
-  result.textContent =
-    `✓ ご予約ありがとうございます、${name}様。${count}名様で承りました。`;
+    // 名前か人数が空だったら、注意メッセージを出して終了する
+   if (name === '' || count === '') {
+        result.textContent = '入力してください';
+        return;
+    } else {
+        result.textContent = `✓ ご予約ありがとうございます、${name}様。${count}名様で承りました。`;
+    }
+} 
+
+// recommend
+function pickRecommend() {
+    
 }
